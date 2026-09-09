9ae4959edb1227213e5719babad6c164c17f9dc3
Daria Strilets <dariastrilets@MacBook-Air-Daria.local>
2026-09-09 15:36:46 +0300

    Add JavaScript primitive data types homework

---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+let firstName = "Tanya";
+let secondName = "Anya";
+const togetherNames = ("Hello " + firstName + " and " + secondName);
+const anotherWayTogether = `Hello again ${firstName} and ${secondName}!`;
+
+console.log (togetherNames);
+console.log (anotherWayTogether);
---
+++ jsBasics/task3.js
@@ -0,0 +1,5 @@
+const age = 56;
+const isAdult = age >= 18;
+
+console.log(age);
+console.log(isAdult);
